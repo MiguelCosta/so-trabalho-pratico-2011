2ea573609bd0aee5ae4a2dad9a1ca0a57bfba6fd
miguelpintodacosta <miguelpintodacosta@5d5945a8-e60f-af1e-5abd-38b2a6cce933>
2011-05-12 14:01:40 +0000

    Repositório do trabalho prático de sistemas operativos iniciado.

---
+++ trunk/SO/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Miguel
+ *
+ * Created on 12 de Maio de 2011, 15:00
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
